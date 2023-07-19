5d1a133ae73c0a83bd0e518084985b6a89ca56f9
Nika Khachiashvili <n.khachiashvili1@gmail.com>
2023-07-19 18:31:54 +0400

    add first deposit tests

--- test/Staking.t.sol
+++ test/Staking.t.sol
@@ -5,6 +5,7 @@ import "forge-std/Test.sol";
 import "forge-std/console.sol";
 
 import "../src/Staking.sol";
+import "../src/Token.sol";
 
 /**
  * @title StakingTest Contract
@@ -12,17 +13,42 @@ import "../src/Staking.sol";
  * @dev Test cases for Staking Contract
  */
 contract StakingTest is Test {
+    uint256 public constant INITIAL_TOKEN_SUPPLY = 1000 ether;
+    uint256 public constant STATIC_INTEREST_RATE = 0.1 ether;
+    uint256 public constant DYNAMIC_REWARD_AMOUNT = 1 ether;
+    uint256 public constant DYNAMIC_REWARD_DURATION = 100 days;
+    bool public constant IS_DYNAMIC = false;
+
     Staking public staking;
+    Token public token;
 
     function setUp() public {
         staking = new Staking();
+        token = new Token("Test Token", "TST", INITIAL_TOKEN_SUPPLY);
         staking.initialize(
-            IERC20Upgradeable(address(0)),
-            0.1 ether,
-            1 ether,
-            100 days,
-            false
+            IERC20Upgradeable(address(token)),
+            STATIC_INTEREST_RATE,
+            DYNAMIC_REWARD_AMOUNT,
+            DYNAMIC_REWARD_DURATION,
+            IS_DYNAMIC
         );
+    }
+
+    /// @dev Test staking when user hasn't previously staked and the staking is static
+    function testInitialStakeStatic() public {
+        uint256 amountToStake = INITIAL_TOKEN_SUPPLY;
+        token.approve(address(staking), amountToStake);
+        staking.stake(amountToStake);
+
+        assertEq(staking.balanceOf(address(this)), 0);
+        assertEq(token.balanceOf(address(this)), 0);
+        assertEq(token.balanceOf(address(staking)), amountToStake);
+        assertEq(staking.totalStaked(), amountToStake);
+
+        Staking.StakerInfo memory staker = staking.getStakerInfo(address(this));
+        assertEq(staker.stakedAmount, amountToStake);
+        assertEq(staker.lastRewardTimestamp, block.timestamp);
+        assertEq(staker.rewardDebt, 0);
     }
 
     /// @dev testing pause functionality

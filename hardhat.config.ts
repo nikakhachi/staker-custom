b6e6bb3cd4065fedb615bcce01f0a5563314949f
Nika Khachiashvili <n.khachiashvili1@gmail.com>
2023-07-21 00:07:12 +0400

    setup hardhat config to mumbai network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,10 +1,23 @@
-import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-foundry";
 import "@openzeppelin/hardhat-upgrades";
 import "@nomicfoundation/hardhat-ethers";
+import "@nomicfoundation/hardhat-verify";
+import "dotenv/config";
 
-const config: HardhatUserConfig = {
-  solidity: "0.8.20",
+const config = {
+  solidity: "0.8.19",
+  networks: {
+    mumbai: {
+      chainId: 80001,
+      url: process.env.MUMBAI_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY || ""],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      polygonMumbai: process.env.POLYGONSCAN_API_KEY,
+    },
+  },
 };
 
 export default config;

daa51e7f1bde9a9e0c54abea3a6c0bb96a0c3683
Nika Khachiashvili <n.khachiashvili1@gmail.com>
2023-07-20 23:29:36 +0400

    add deployment scripts

--- script/deploy.ts
+++ script/deploy.ts
@@ -1,6 +1,24 @@
 import { upgrades, ethers } from "hardhat";
 
-const main = async () => {};
+const main = async () => {
+  const TokenFactory = await ethers.getContractFactory("Token");
+  const token = await TokenFactory.deploy("Test Token", "TST", ethers.parseEther("1000000"));
+
+  await token.waitForDeployment();
+
+  const tokenAddress = await token.getAddress();
+
+  console.log(`Token Deployed on Address: ${tokenAddress}`);
+
+  const StakingFactory = await ethers.getContractFactory("Staking");
+  const staking = await upgrades.deployProxy(StakingFactory, [tokenAddress, false, false], { kind: "uups" });
+
+  await token.waitForDeployment();
+
+  const stakingAddress = await staking.getAddress();
+
+  console.log(`Staking Deployed on Address: ${stakingAddress}`);
+};
 
 main().catch((error) => {
   console.error(error);

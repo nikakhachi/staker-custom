95d72389b91cf7bd04f1d49f86081b53da3c9d99
Nika Khachiashvili <n.khachiashvili1@gmail.com>
2023-07-20 16:10:11 +0400

    add 2 test cases for dynamic staking

--- test/DynamicStaking.t.sol
+++ test/DynamicStaking.t.sol
@@ -16,7 +16,7 @@ contract StakingTest is Test {
     uint256 public constant INITIAL_TOKEN_SUPPLY = 1000 ether;
     uint256 public constant STATIC_INTEREST_RATE = 0.1 ether;
     uint256 public constant DYNAMIC_REWARD_AMOUNT = 10000 ether;
-    uint256 public constant DYNAMIC_REWARD_DURATION = 100 days;
+    uint256 public constant DYNAMIC_REWARD_DURATION = 1000000; /// @dev 1000000 seconds = 11.5 days
     bool public constant IS_DYNAMIC = true;
 
     Staking public staking;
@@ -31,6 +31,53 @@ contract StakingTest is Test {
             DYNAMIC_REWARD_AMOUNT,
             DYNAMIC_REWARD_DURATION,
             IS_DYNAMIC
+        );
+    }
+
+    /// @dev Test rewards for a staker if only 1 staker is present from the start to the end
+    function testDynamicStakingFlowWithOneStaker() public {
+        uint256 amountToStake = INITIAL_TOKEN_SUPPLY;
+        token.approve(address(staking), amountToStake);
+        staking.stake(amountToStake);
+
+        assertEq(staking.lastUpdateTime(), block.timestamp);
+
+        skip(DYNAMIC_REWARD_DURATION);
+
+        assertEq(staking.balanceOf(address(this)), 0);
+        assertEq(token.balanceOf(address(this)), 0);
+        assertEq(token.balanceOf(address(staking)), amountToStake);
+        assertEq(staking.totalStaked(), amountToStake);
+
+        assertEq(
+            staking.viewPendingRewards(address(this)),
+            DYNAMIC_REWARD_AMOUNT
+        );
+    }
+
+    /// @dev Test rewards for a 2 concurrent stakers if only 2 stakers are present from the start to the end
+    function testDynamicStakingFlowWithTwoConcurrentStaker() public {
+        uint256 amountToStake = 1 ether;
+        address staker2 = address(1);
+        token.transfer(staker2, amountToStake);
+
+        token.approve(address(staking), amountToStake);
+        staking.stake(amountToStake);
+
+        vm.prank(staker2);
+        token.approve(address(staking), amountToStake);
+        vm.prank(staker2);
+        staking.stake(amountToStake);
+
+        skip(DYNAMIC_REWARD_DURATION);
+
+        assertEq(
+            staking.viewPendingRewards(address(this)),
+            DYNAMIC_REWARD_AMOUNT / 2
+        );
+        assertEq(
+            staking.viewPendingRewards(staker2),
+            DYNAMIC_REWARD_AMOUNT / 2
         );
     }
 }
